256d9ef83195686a6ed2122b615be50a8f123242
Kshitij Verma <kshitijv250@gmail.com>
2023-04-09 00:01:20 +0530

    add and delete methods implemented

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,33 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import InputField from "./InputField";
 
 const formItems = [
   {
-    key: "1",
+    id: 1,
     label: "First Name",
     type: "text",
     placeholder: "Enter your first name",
   },
   {
-    key: "2",
+    id: 2,
     label: "Last Name",
     type: "text",
     placeholder: "Enter your last name",
   },
   {
-    key: "3",
+    id: 3,
     label: "Email",
     type: "email",
     placeholder: "Enter your email",
   },
   {
-    key: "4",
+    id: 4,
     label: "Phone Number",
     type: "tel",
     placeholder: "Enter your phone number",
   },
   {
-    key: "5",
+    id: 5,
     label: "Date of Birth",
     type: "date",
     placeholder: "Enter your date of birth",
@@ -35,12 +35,54 @@ const formItems = [
 ];
 
 export default function Form(props: { closeFormCB: () => void }) {
+  const [formState, setFormState] = useState(formItems);
+  const [fieldValue, setFieldValue] = useState("");
+
+  const addField = () => {
+    setFormState([
+      ...formState,
+      {
+        id: Number(new Date()),
+        label: fieldValue,
+        type: "text",
+        placeholder: "",
+      },
+    ]);
+    setFieldValue("");
+  };
+  const removeField = (id: number) => {
+    setFormState(formState.filter((item) => item.id !== id));
+  };
+
   return (
-    <form className="flex flex-col gap-4 p-2">
-      {formItems.map((item) => (
-        <InputField item={item} key={item.key} />
-      ))}
-      <div className="flex flex-row gap-2">
+    <div className="flex flex-col gap-4 p-2 divide-y divide-double divide-gray-300">
+      <div className="flex-1">
+        {formState.map((item) => (
+          <InputField
+            removeField={removeField}
+            item={item}
+            key={item.id}
+            id={item.id}
+          />
+        ))}
+      </div>
+      <div className="flex gap-2 pt-4">
+        <input
+          type="text"
+          onChange={(e) => {
+            setFieldValue(e.target.value);
+          }}
+          value={fieldValue}
+          className="flex-1 border-2 rounded-lg border-gray-300 p-2 focus:border-cyan-500 focus:outline-none"
+        />
+        <button
+          className="bg-cyan-500 text-white p-2 rounded-md w-fit"
+          onClick={addField}
+        >
+          Add Field
+        </button>
+      </div>
+      <div className="flex flex-row gap-2 pt-2">
         <button className="bg-cyan-500 text-white p-2 rounded-md w-fit">
           Submit
         </button>
@@ -51,6 +93,6 @@ export default function Form(props: { closeFormCB: () => void }) {
           Close Form
         </button>
       </div>
-    </form>
+    </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,16 +1,27 @@
 import React from "react";
+import deleteIcon from "../delete.svg";
 
 export default function InputField(props: {
   item: { label: string; type: string; placeholder: string };
+  removeField: (id: number) => void;
+  id: number;
 }) {
   return (
     <div className="flex flex-col gap-2">
       <label className="text-sm font-semibold">{props.item.label}</label>
-      <input
-        className="border-2 rounded-lg border-gray-300 p-2 focus:border-cyan-500 focus:outline-none"
-        type={props.item.type}
-        placeholder={props.item.placeholder}
-      />
+      <div className="flex w-full gap-2">
+        <input
+          className="border-2 rounded-lg border-gray-300 p-2 focus:border-cyan-500 focus:outline-none w-full"
+          type={props.item.type}
+          placeholder={props.item.placeholder}
+        />
+        <button
+          className="bg-cyan-500 text-white p-2 rounded-md w-fit"
+          onClick={(_) => props.removeField(props.id)}
+        >
+          {<img src={deleteIcon} alt="delete" className="w-8" />}
+        </button>
+      </div>
     </div>
   );
 }
