9bcb1d362c09b688eb7af719518a971bbe8c5cbc
Kshitij Verma <kshitijv250@gmail.com>
2023-04-18 23:51:41 +0530

    input stored in the state

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import { MyForm } from "./data";
+import { FormItem } from "./data";
 
 export default function Preview(props: { formId: Number }) {
   const forms = localStorage.getItem("forms");
   const form =
     forms && JSON.parse(forms).find((form: MyForm) => form.id === props.formId);
   const [state, setState] = useState(0);
+  const [formState, setFormState] = useState<FormItem[]>(form.fields);
+
   return (
     <div className="p-4 flex flex-col">
       <div className="flex justify-between items-center">
@@ -19,12 +22,18 @@ export default function Preview(props: { formId: Number }) {
           <input
             className="border-2 rounded-lg border-gray-300 p-2 focus:border-cyan-500 focus:outline-none w-full"
             type={form.fields[state].type}
+            value={formState[state].value}
+            onChange={(e) => {
+              const newState = [...formState];
+              newState[state].value = e.target.value;
+              setFormState(newState);
+            }}
           />
         </div>
       </div>
-      <div className="flex justify-between items-center mt-4">
+      <div className="flex justify-between items-center mt-4 gap-4">
         <button
-          className="bg-cyan-500 text-white p-2 rounded-md w-fit"
+          className="bg-cyan-500 text-white p-2 rounded-md w-full"
           onClick={(_) => {
             if (state > 0) {
               setState(state - 1);
@@ -34,7 +43,7 @@ export default function Preview(props: { formId: Number }) {
           Previous
         </button>
         <button
-          className="bg-cyan-500 text-white p-2 rounded-md w-fit"
+          className="bg-cyan-500 text-white p-2 rounded-md w-full"
           onClick={(_) => {
             if (state < form.fields.length - 1) {
               setState(state + 1);
