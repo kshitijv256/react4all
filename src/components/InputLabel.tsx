7476ce4d7536d9eb7878cc397959d039ecfdd426
Kshitij Verma <kshitijv250@gmail.com>
2023-05-23 14:29:33 +0530

    made input label editable

--- src/components/InputLabel.tsx
+++ src/components/InputLabel.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import deleteIcon from "../assets/delete.svg";
+import editIcon from "../assets/edit.svg";
 import { FormItem } from "../types/data";
 
 export default function InputLabel(props: {
@@ -8,18 +9,28 @@ export default function InputLabel(props: {
   changedCB: (newForm: FormItem, id: number) => void;
   id: number;
 }) {
+  const [label, setLabel] = useState(props.item.label);
+  const updateLabel = () => {
+    const newForm = { ...props.item, label: label };
+    props.changedCB(newForm, props.id);
+  };
   return (
     <div className="flex flex-col gap-2">
       <div className="flex w-full gap-2">
         <input
           className="border-2 rounded-lg border-gray-300 p-2 focus:border-cyan-500 focus:outline-none w-full"
           type="text"
-          value={props.item.label}
+          value={label}
           onChange={(e) => {
-            const newForm = { ...props.item, label: e.target.value };
-            props.changedCB(newForm, props.id);
+            setLabel(e.target.value);
           }}
         />
+        <button
+          className="bg-cyan-500 text-white p-2 rounded-md w-fit"
+          onClick={(_) => updateLabel()}
+        >
+          {<img src={editIcon} alt="delete" className="w-8" />}
+        </button>
         <button
           className="bg-cyan-500 text-white p-2 rounded-md w-fit"
           onClick={(_) => props.removeFieldCB(props.id)}
