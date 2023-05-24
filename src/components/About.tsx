1a10d91798be3dd816157d07db5c78c29a3c9ee5
Kshitij Verma <kshitijv250@gmail.com>
2023-05-24 18:04:21 +0530

    minor ui fix

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function About() {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center gap-4">
       <div className="text-2xl font-bold">About</div>
       <div className="text-lg">
         This is a submission for Level 7 of the React for All course.
