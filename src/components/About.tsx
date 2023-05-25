3ae6341b05e3b533034fd91709d82ed747cf90e9
Kshitij Verma <kshitijv250@gmail.com>
2023-05-25 21:27:55 +0530

    added details to about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,10 +2,22 @@ import React from "react";
 
 export default function About() {
   return (
-    <div className="flex flex-col items-center gap-4">
+    <div className="flex flex-col gap-4 mt-4 text-gray-700">
       <div className="text-2xl font-bold">About</div>
       <div className="text-lg">
         This is a submission for Level 8 of the React for All course.
+      </div>
+      <div>
+        New in this submission:
+        <ul className="list-disc ml-6 list">
+          <li className="">Checked keyboard accessibilty of app</li>
+          <li className="">
+            Added shareable link using <b>react-copy-to-clipboard</b> package
+          </li>
+          <li>
+            Added <b>react-qr-code</b> package to generate QR code for link
+          </li>
+        </ul>
       </div>
       <div className="text-lg">
         This project is made by{" "}
