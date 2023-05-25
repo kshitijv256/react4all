f554f2c0be0af94cdd7f21f723adba46f8f65677
Kshitij Verma <kshitijv250@gmail.com>
2023-05-25 21:12:44 +0530

    added qr code generator for shareable link

--- src/components/common/ShareForm.tsx
+++ src/components/common/ShareForm.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { useState } from "react";
+import QRCode from "react-qr-code";
+import closeIcon from "../../assets/close.svg";
+import { Link } from "raviger";
 
 export default function ShareForm(props: { formID: number }) {
   const [copied, setCopied] = useState(false);
+  const link: string = `localhost:3000/preview/${props.formID}`;
 
   const onCopy = () => {
     setCopied(true);
@@ -14,21 +18,30 @@ export default function ShareForm(props: { formID: number }) {
 
   return (
     <div className="flex flex-col items-center mt-4">
+      <div className="flex item-center w-full justify-start">
+        <Link href={`/form/${props.formID}`}>
+          <img src={closeIcon} alt="close" className="h-10" />
+        </Link>
+      </div>
       <div className="flex item-center w-full justify-end">
-        <CopyToClipboard
-          text={`localhost:3000/preview/${props.formID}`}
-          onCopy={onCopy}
-        >
-          <button className="p-2 bg-green-500 rounded-md text-white hover:bg-green-400">
+        <CopyToClipboard text={link} onCopy={onCopy}>
+          <button className="p-2 bg-cyan-500 rounded-md text-white hover:bg-cyan-400">
             Copy Link
           </button>
         </CopyToClipboard>
         {copied && (
-          <span className="text-gray-200 bg-black/50 p-2 text-sm rounded fixed right-0">
+          <span className="text-gray-200 bg-black/50 p-2 text-sm rounded fixed mt-12">
             Copied!
           </span>
         )}
       </div>
+      <QRCode
+        size={256}
+        style={{ height: "300px", maxWidth: "100%", width: "100%" }}
+        value={link}
+        viewBox={`0 0 256 256`}
+      />
+      <p className="mt-6 text-lg">Scan QR code to preview</p>
     </div>
   );
 }
