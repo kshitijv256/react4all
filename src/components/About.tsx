577f479840a53e281364e59b5a10849ccac8287e
Kshitij Verma <kshitijv250@gmail.com>
2023-05-27 14:51:48 +0530

    updated about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,13 +11,14 @@ export default function About() {
         New in this submission:
         <ul className="list-disc ml-6 list">
           <li className="">
-            Added sentry for error reporting and performance monitoring
+            Integrated sentry for error reporting and performance monitoring
           </li>
           <li>
             Added Location picker from source without installing{" "}
             <b>react-google-map-picker-custom</b> package
           </li>
           <li>Deployed on Netlify</li>
+          <li>Made Web app a PWA</li>
         </ul>
       </div>
       <div className="text-lg">
